1896950cfcc4df029f8809caf22c27e79a2ab28e
rsoulard <soulard1ra@gmail.com>
2024-04-17 16:49:16 -0400

    Update tests - Update AppComponent tests remove unecessary tests. - Update BoardComponent tests to test for creation. - Update BoardService tests to add tests for retrieve and list methods.

--- RetroSpec.Web/projects/retro-spec-web/src/app/routes/board/board.component.spec.ts
+++ RetroSpec.Web/projects/retro-spec-web/src/app/routes/board/board.component.spec.ts
@@ -1,16 +1,67 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BoardComponent } from './board.component';
+import { of } from 'rxjs';
+import { BoardService } from '../../shared/domain/services/board.service';
+import { CardService } from '../../shared/domain/services/card.service';
+import { provideRouter } from '@angular/router';
+import { RouterTestingHarness } from '@angular/router/testing';
 
 describe('BoardComponent', () => {
   let component: BoardComponent;
   let fixture: ComponentFixture<BoardComponent>;
+  let boardRetrieveSpy;
+  let cardListSpy;
 
   beforeEach(async () => {
+
+    const boardService = jasmine.createSpyObj('BoardService', [
+      'retrieve'
+    ]);
+    boardRetrieveSpy = boardService.retrieve.and.returnValue(of({
+      id: '00000000-0000-0000-0000-000000000000',
+      name: 'Test Board',
+      columns: [
+        {
+          id: 0,
+          name: 'Column 1'
+        },
+        {
+          id: 1,
+          name: 'Column 2'
+        }
+      ]
+    }));
+
+    const cardService = jasmine.createSpyObj('CardService', [
+      'list'
+    ]);
+    cardListSpy = cardService.list.and.returnValue(of([
+      {
+        id: '00000000-0000-0000-0000-000000000001',
+        columnId: 0,
+        body: 'Card 1'
+      },
+      {
+        id: '00000000-0000-0000-0000-000000000002',
+        columnId: 1,
+        body: 'Card 2'
+      }
+    ]));
+
     await TestBed.configureTestingModule({
-      imports: [BoardComponent]
+      imports: [BoardComponent],
+      providers: [
+        provideRouter([{path: '**', component: BoardComponent}]),
+        { provide: BoardService, useValue: boardService },
+        { provide: CardService, useValue: cardService}
+      ]
     })
     .compileComponents();
-    
+
+    const harness = await RouterTestingHarness.create();
+    await harness.navigateByUrl('/', BoardComponent);
+    harness.detectChanges();
+
     fixture = TestBed.createComponent(BoardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- RetroSpec.Web/projects/retro-spec-web/src/app/shared/domain/services/board.service.spec.ts
+++ RetroSpec.Web/projects/retro-spec-web/src/app/shared/domain/services/board.service.spec.ts
@@ -1,16 +1,80 @@
-import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed, inject } from '@angular/core/testing';
+import { environment } from '../../../../environments/environment';
 import { BoardService } from './board.service';
 
 describe('BoardService', () => {
   let service: BoardService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ]
+    });
     service = TestBed.inject(BoardService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should call retrieve endpoint', inject(
+    [HttpTestingController, BoardService],
+    (mockHttp: HttpTestingController, boardService: BoardService) => {
+      const mockBoard = {
+        id: '00000000-0000-0000-0000-000000000000',
+        name: 'Test',
+        columns: [
+          {
+            id: 0,
+            name: 'Column 1'
+          },
+          {
+            id: 1,
+            name: 'Column 2'
+          }
+        ]
+      };
+
+      boardService.retrieve('00000000-0000-0000-0000-000000000000')
+        .subscribe(response => {
+          expect(response).toEqual(mockBoard);
+        });
+
+      const mockRequest = mockHttp.expectOne(`${environment.apiUrl}/board/00000000-0000-0000-0000-000000000000`);
+
+      expect(mockRequest.cancelled).toBeFalsy();
+      expect(mockRequest.request.method).toEqual('GET');
+      mockRequest.flush(mockBoard);
+
+      mockHttp.verify();
+    }
+  ));
+
+  it('should call list endpoint', inject(
+    [HttpTestingController, BoardService],
+    (mockHttp: HttpTestingController, boardService: BoardService) => {
+      const mockBoards = [
+        {
+          id: '00000000-0000-0000-0000-000000000001',
+          name: 'Test 1'
+        },
+        {
+          id: '00000000-0000-0000-0000-000000000002',
+          name: 'Test 2'
+        }
+      ];
+
+      boardService.list('00000000-0000-0000-0000-000000000000')
+        .subscribe(response => {
+          expect(response).toEqual(mockBoards);
+        });
+
+      const mockRequest = mockHttp.expectOne(`${environment.apiUrl}/team/00000000-0000-0000-0000-000000000000/board`);
+      expect(mockRequest.cancelled).toBeFalsy();
+      expect(mockRequest.request.method).toEqual('GET');
+      mockRequest.flush(mockBoards);
+
+      mockHttp.verify();
+    }
+  ));
 });
